70aab41a1062dc4063c6a2a02560be715241ea16
Austin Nelson <nel.austintyler.son@gmail.com>
2022-01-10 12:12:36 -0700

    dynamic copyright year

--- wdd230/w01/js/date.js
+++ wdd230/w01/js/date.js
@@ -1,2 +1,5 @@
 let x = document.lastModified;
 document.getElementById('date').innerHTML = `Last Updated: ${x}`;
+
+let currentYear = new Date().getFullYear();
+document.getElementById('year').innerHTML = currentYear;
