21248935c11bf77b6d68177289f21e782d501dce
Austin Nelson <nel.austintyler.son@gmail.com>
2022-04-30 13:50:04 -0600

    last update

--- wdd230/lesson2/js/date.js
+++ wdd230/lesson2/js/date.js
@@ -1,2 +1,5 @@
 let currentYear = new Date().getFullYear();
 document.getElementById('year').innerHTML = currentYear;
+
+let x = document.lastModified;
+document.getElementById('date').innerHTML = `Last Updated: ${x}`;
