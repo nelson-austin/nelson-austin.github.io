a6e96be4cddc5144ceca24fe82ce06a69bfe3534
Austin Nelson <nel.austintyler.son@gmail.com>
2022-05-21 23:06:21 -0600

    main changes

--- wdd230/chamber/js/designs.js
+++ wdd230/chamber/js/designs.js
@@ -35,38 +35,38 @@ document.getElementById('modified').innerHTML = `Last Updated: ${y}`;
 
 
 
-// function weather() {
+function weather() {
 
-//     const apiKey = 'fddb75827544bb7915ab737cd8bd0e60';
+    const apiKey = 'fddb75827544bb7915ab737cd8bd0e60';
 
-//     const url = `https://api.openweathermap.org/data/2.5/weather?q=phoenix&appid=${apiKey}&units=metric`;
-//     fetch(url)
-//     .then(response => response.json())
-//     .then(data => {
-//         const { main, name, sys, weather } = data;
-//         const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
-//         weather[0]["icon"]
-//         }.svg`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=phoenix&appid=${apiKey}&units=metric`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const { main, name, sys, weather } = data;
+        const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
+        weather[0]["icon"]
+        }.svg`;
 
-//         const li = document.createElement("li");
-//         li.classList.add("city");
-//         const markup = `
-//         <h2 class="city-name" data-name="${name},${sys.country}">
-//             <span>${name}</span>
-//             <sup>${sys.country}</sup>
-//         </h2>
-//         <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
-//         <figure>
-//             <img class="city-icon" src="${icon}" alt="${
-//         weather[0]["description"]
-//         }">
-//             <figcaption>${weather[0]["description"]}</figcaption>
-//         </figure>
-//         `;
-//         li.innerHTML = markup;
-//         document.querySelector('.cities').appendChild(li);
-//     })
+        const li = document.createElement("li");
+        li.classList.add("city");
+        const markup = `
+        <h2 class="city-name" data-name="${name},${sys.country}">
+            <span>${name}</span>
+            <sup>${sys.country}</sup>
+        </h2>
+        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+        <figure>
+            <img class="city-icon" src="${icon}" alt="${
+        weather[0]["description"]
+        }">
+            <figcaption>${weather[0]["description"]}</figcaption>
+        </figure>
+        `;
+        li.innerHTML = markup;
+        document.querySelector('.cities').appendChild(li);
+    })
 
-// }
+}
 
-// weather();
+weather();
