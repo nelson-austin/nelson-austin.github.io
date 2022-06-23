1c53a71d31cffb59682c3b16e5699ae32e10d237
Austin Nelson <nel.austintyler.son@gmail.com>
2022-06-22 20:13:24 -0600

    main changes

--- wdd230/chamber/js/directory.js
+++ wdd230/chamber/js/directory.js
@@ -16,31 +16,41 @@ fetch(file)
   function displayBusinesses(business) {
     // Create elements to add to the document
     let card = document.createElement('section');
+    let div = document.createElement('div');
     let h2 = document.createElement('h2');
     let portrait = document.createElement('img');
+    let a = document.createElement('a');
+    let a2 = document.createElement('a');
     let p1 = document.createElement('p');
     let p2 = document.createElement('p');
     let p3 = document.createElement('p');
-    let p4 = document.createElement('p');
   
     // Change the textContent property of the h2 element to contain the prophet's full name
     h2.textContent = `${business.name}`;
     p1.textContent = `Address: ${business.address}`;
     p2.textContent = `Phone Number: ${business.phonenumber}`;
-    p3.textContent = `${business.websiteurl}`;
-    p4.textContent = `Membership level: ${business.membershiplevel}`;
+    a.href = `${business.websiteurl}`;
+    a.target = "_blank";
+    a2.textContent = `Link to ${business.name}`;
+    a2.href = `${business.websiteurl}`;
+    a2.target = "_blank";
+    p3.textContent = `Membership level: ${business.membershiplevel}`;
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
     portrait.setAttribute('src', `${business.icon}`);
     portrait.setAttribute('alt', `Icon for ${business.icon}`);
     portrait.setAttribute('loading', 'lazy');
+    a2.classList.add('tinyurl');
   
     // Add/append the section(card) with the h2 element
+    card.appendChild(div);
+    div.appendChild(a);
+    a.appendChild(portrait);
     card.appendChild(h2);
     card.appendChild(p1);
     card.appendChild(p2);
     card.appendChild(p3);
-    card.appendChild(p3);
-    card.appendChild(portrait);
+    card.appendChild(a2);
+    
   
     // Add/append the existing HTML div with the cards class with the section(card)
     document.querySelector('div.cards').appendChild(card);
