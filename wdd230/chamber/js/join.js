7202ed92c4b0610cd418ef83a141d8a525930c1f
Austin Nelson <nel.austintyler.son@gmail.com>
2022-07-02 00:14:09 -0600

    appearance

--- wdd230/chamber/js/join.js
+++ wdd230/chamber/js/join.js
@@ -2,3 +2,26 @@
 const then = new Date();
 const when = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
 document.getElementById('formdate').value = when;
+
+const firstName = document.getElementById('firstname');
+const lastName = document.getElementById('lastname');
+const email = document.getElementById('email');
+const number = document.getElementById('number');
+const businessName = document.getElementById('businessname');
+
+
+firstName.addEventListener('blur', () => {
+    firstName.classList.add('error');
+})
+lastName.addEventListener('blur', () => {
+    lastName.classList.add('error');
+})
+email.addEventListener('blur', () => {
+    email.classList.add('error');
+})
+number.addEventListener('blur', () => {
+    number.classList.add('error');
+})
+businessName.addEventListener('blur', () => {
+    businessName.classList.add('error');
+})
